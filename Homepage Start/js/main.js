323d49753c352927ae1d885cf5f9c729f462e973
Alex <bremard.alexandre@gmail.com>
2020-11-06 11:02:28 +0100

    JS services listing game

--- Homepage Start/js/main.js
+++ Homepage Start/js/main.js
@@ -1,2 +1,51 @@
+function searchButton() {
+    search = document.getElementById("search").value;
+    window.location.href = "./result.html?search="+search;
+    sparqlQuery = buildQuery(search);
+}
 
+function buildQuery() {
+    search = window.location.search.split("=")[1];
+    console.log("building query");
+    sparqlQuery = "SELECT * WHERE {?jv a dbo:VideoGame; foaf:name ?name. FILTER ( regex(?jv, 'Mario.*', 'i') )} LIMIT 100";
+    sparqlQuery = encodeURIComponent(sparqlQuery);
+    // jsonResponse = sendRequest(sparqlQuery).then((req, res) => {
+    //     console.log("fin reponse");
+    //     console.log(jsonResponse);
+    //     jsonParseGameList(jsonResponse);
+    // });
+    jsonResponse = sendRequest(sparqlQuery);
+    console.log(jsonResponse);
+    jsonParseGameList(jsonResponse);
+    return sparqlQuery;
+}
 
+function sendRequest(sparqlQuery) {
+    var url = "http://dbpedia.org/sparql/?default-graph-uri=http%3A%2F%2Fdbpedia.org&query="+sparqlQuery+"&format=JSON";
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false ); // false for synchronous request
+    xmlHttp.send( null );
+    response = JSON.parse(xmlHttp.response);
+    return response;
+}
+
+function getGame(uri) {
+    var url = uri;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", url, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+function jsonParseGameList(jsonObject) {
+    console.log(jsonObject.results.bindings);
+    jsonObject.results.bindings.forEach(elem => {
+        var name = elem.name.value;
+        var uri = elem.jv.value;
+        document.getElementById("resultTable").innerHTML += "<tr><td onclick=retrieveDetails("+uri+")>"+name+"</td></tr>";
+    });
+}
+
+function retrieveDetails(uri) {
+    
+}
